8c6d89c51f68ed0608708725c4a842ab3ff5c409
Caner Tüysüz <canertuysuz@hotmail.com>
2024-09-25 12:52:07 +0300

    update 01_values

--- 01_values.js
+++ 01_values.js
@@ -8,10 +8,14 @@
 console.log(typeof 4.5, typeof "x")
 
 //? Comparison
-console.log("A" < "Z") // True
-console.log("a" < "z") // True
-console.log("a" > "Z") // True
-console.log(NaN !== NaN) // True
+console.log("A" < "Z")
+// -> True
+console.log("a" < "z")
+// -> True
+console.log("a" > "Z")
+// -> True
+console.log(NaN !== NaN)
+// -> True
 
 /* 
 NaN is supposed to denote the result of a nonsensical 
@@ -20,15 +24,20 @@ other nonsensical computations.
 */
 
 //? Logical Operators
-console.log(true && false) // False
-console.log(false || true) // True
+console.log(true && false)
+// -> False
+console.log(false || true)
+// -> True
 
 // Due to a historical quirk, null is type of object in javascript
 
 //? Type Coercion
-console.log(8 * null) //0
-console.log("5" - 1) // 4
-console.log("5" + 1) // 51
+console.log(8 * null)
+// → 0
+console.log("5" - 1)
+// → 4
+console.log("5" + 1)
+// → 51
 console.log("five" * 2)
 // → NaN
 console.log(false == 0)
@@ -37,3 +46,15 @@ console.log(null == undefined)
 // → true
 
 //? Short-circuiting of logical operators
+
+// || Operator return teh value to it's left when that value
+// can be converted to true. And on it's right otherwise.
+console.log(null || "user")
+// -> user
+console.log("Agnes" || "user")
+// -> Agnes
+
+//! 0, NaN, and the empty string ("") counted as false
+
+//! X will be ignored and not being evaluated in those cases:
+//! true || X, false && X
